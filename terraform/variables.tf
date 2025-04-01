fe157e4225371d2f0615c56f2fd2f84d40f1084e
AngelitoMA11 <angelitoma200211@gmail.com>
2025-04-01 16:42:58 +0200

    Plantilla_terraform
    Inicio del terraform con su estructura.

---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "project_id" {
+  description = "ID del proyecto de GCP."
+  type        = string
+}
+  
+variable "zone" {
+  description = "Zona del proyecto"
+  type        = string
+}
+
+variable "region" {
+  description = "Zona del proyecto"
+  type        = string
+}
