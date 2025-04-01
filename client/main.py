379b43aef9b9deea495e7dbc9b293ddff118c50b
fab1an12 <fabianmiulescu@gmail.com>
2025-04-01 23:46:05 +0200

    Refactor client code to consolidate utility functions and improve public IP retrieval; remove deprecated network_info module.

--- client/main.py
+++ client/main.py
@@ -1,9 +1,6 @@
 import time
 import json
-from mac_checker import get_mac_address
-from network_info import get_public_ip
-from database import insert_user_data, get_user_data
-from api_client import send_data_to_endpoint
+from utils.utils import get_mac_address, get_public_ip, is_mac_registered, insert_user_data
 
 def main():
     mac_address = get_mac_address()
@@ -15,26 +12,19 @@ def main():
         grade = input("Enter your grade: ")
         academic_year = input("Enter your academic year: ")
         insert_user_data(mac_address, name, surname, email, grade, academic_year)
-    
-    user_data = get_user_data(mac_address)
-    
+        
     while True:
         ip_address = get_public_ip()
         timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         
         data_to_send = {
-            "mac_address": user_data.mac_address,
-            "name": user_data.name,
-            "surname": user_data.surname,
-            "email": user_data.email,
-            "grade": user_data.grade,
-            "academic_year": user_data.academic_year,
+            "mac_address": mac_address,
             "public_ip": ip_address,
             "timestamp": timestamp
         }
         
         send_data_to_endpoint(data_to_send)
-        time.sleep(30)
+        time.sleep(15)
 
 if __name__ == "__main__":
     main()
